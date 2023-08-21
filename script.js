42606e987b5136b989622098c00ed27783a2ea0c
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-21 19:30:56 -0300

    Defined more constants

--- script.js
+++ script.js
@@ -41,3 +41,28 @@ const generatePassword = () => {
   passwordInput.value = randomPassword;
 
 }
+
+const updatePassIndicator = () => {
+  passIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong";
+}
+
+const updateSlider = () => {
+  document.querySelector(".pass-length span").innerText = lengthSlider.value;
+  generatePassword();
+  updatePassIndicator();
+}
+updateSlider();
+
+const copyPassword = () => {
+  navigator.clipboard.writeText(passwordInput.value);
+  copyIcon.innerText = "check";
+  copyIcon.style.color = "#4285f4";
+  setTimeout(() => {
+    copyIcon.innerText = "copy_all";
+    copyIcon.style.color = "#707070";
+  }, 1500);
+}
+
+copyIcon.addEventListener("click", copyPassword);
+lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
